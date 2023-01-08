6c0f554b9effd8c7d05f307c24985324947d5528
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-01-08 19:59:54 +0530

    Ability to create contacts in address Book

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package Bridgelabz.com;
+
+public class MainClass {
+    public static void main(String[] args) {
+        AddressBook obj = new AddressBook ();
+        obj.createContacts();
+    }
+}
